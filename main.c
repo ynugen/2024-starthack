490386e9c8b1afb31233a79537e53021d3970cc8
lsammann <161778450+lsammann@users.noreply.github.com>
2024-07-03 19:34:26 +1000

    reading through weights with correct concatenation

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 
 #define WEIGHTS_AND_BIASES_DIR "./weights_and_biases.txt"
-#define BUFFSIZE 1024
+#define BUFFSIZE 8192
 
 
 typedef struct {
@@ -15,6 +17,7 @@ typedef struct {
 
 
 weight_and_bias_t* readWeightsAndBiases();
+bool needsConcatenation(char *finalToken, char *token);
 
 
 
@@ -48,17 +51,69 @@ weight_and_bias_t* readWeightsAndBiases() {
         exit(EXIT_FAILURE);
     }
 
-    char buffer[BUFFSIZE];
     int count = 0;
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    char buffer[BUFFSIZE];
+    size_t bytesRead;
 
-        printf("%s", buffer);
-        if (++count == 3) {
-            break;
+    const char delim[] = ",\n"; // delimiters to split
+    char *token; // array of tokens
+    char finalToken[30];
+
+    int bytesScanned = 0;
+    bool firstIteration = true;
+
+    // main loop to process entire file of weights and biases
+    while ((bytesRead = fread(buffer, 1, BUFFSIZE, fp)) > 0) {
+
+        bytesScanned = 0;
+        // check if this is the first iteration
+        if (firstIteration) {
+
+            token = strtok(buffer, delim);
+            bytesScanned += strlen(token) + 1;
+            firstIteration = false;
+
+        } else { // otherwise check if the first token in the buffer is fully intact
+            
+            token = strtok(buffer, delim);
+            bytesScanned += strlen(token) + 1;
+
+            // check if the strings need to be concatenated
+            if (needsConcatenation(finalToken, token)) {
+                strcat(finalToken, token);
+                printf("concatenated = %s\n", finalToken);
+            }
         }
+        
+        while (bytesScanned < bytesRead) {
+            token = strtok(NULL, delim);
+            bytesScanned += strlen(token) + 1; // +1 for each delimiter
+            //printf("token = \"%s\", bytes scanned = %d\n", token, bytesScanned);
+        }
+        strcpy(finalToken, token);
+
+        //fwrite(buffer, 1, bytesRead, stdout);
+        /*if (count++ == 5) {
+            break;
+        }*/
     }
+   
     fclose(fp);
 
     return wab;
 }
 
+bool needsConcatenation(char *finalToken, char *token) {
+
+    printf("\nCHECKING CONCATENATION final is %s, new is %s\n", finalToken, token);
+
+    if (strlen(finalToken) == 1 && finalToken[0] == '-') { // if previous token was '-' then needs to be concatenated
+        return true;
+    }
+
+    if (token[1] == '.' || token[0] == '-') { // intact if either _.___ or -____
+        return false;
+    }
+
+    return  true; // presume not intact
+}
